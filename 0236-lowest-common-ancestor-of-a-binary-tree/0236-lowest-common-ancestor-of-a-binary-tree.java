4a76eafecd196947f6fc33dbe696f5d9657c0419
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2022-10-28 03:03:16 +0530

    Time: 16 ms (44.39%), Space: 48.5 MB (10.64%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,17 +9,33 @@
  */
 class Solution {
      
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root.val == p.val || root.val == q.val) return root;
+   public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ArrayList<TreeNode> l1 = new ArrayList<>();
+        ArrayList<TreeNode> l2 = new ArrayList<>();   
         
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        isPresent(root, p.val, l1);
+        isPresent(root, q.val, l2);
         
-        if(right == null) return left;
-        if(left == null) return right;
+        int i = l1.size()-1;
+        int j = l2.size()-1;
         
-        return root;
+        while(i >= 0 && j >= 0 && l1.get(i).val == l2.get(j).val) {
+            i--;
+            j--;
+        }
+        
+        if(i == l1.size()-1) return null;
+        return l1.get(i+1);
+        
+    }
+    
+    public boolean isPresent(TreeNode root, int target, ArrayList<TreeNode> ans) {
+        if(root == null) return false;
+        if(root.val == target || isPresent(root.left, target, ans) || isPresent(root.right, target, ans)) {
+            ans.add(root);
+            return true;
+        }
+        return false;
     }
     
     
