d09d34fb193a2c468e12730662921df7057e32eb
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2022-10-30 03:43:36 +0530

    Time: 27 ms (10.70%), Space: 47.2 MB (57.29%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,32 +10,42 @@
 class Solution {
      
    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> l1 = new ArrayList<>();
-        ArrayList<TreeNode> l2 = new ArrayList<>();   
+//         ArrayList<TreeNode> l1 = new ArrayList<>();
+//         ArrayList<TreeNode> l2 = new ArrayList<>();   
         
-        isPresent(root, p.val, l1);
-        isPresent(root, q.val, l2);
+//         isPresent(root, p.val, l1);
+//         isPresent(root, q.val, l2);
         
-        int i = l1.size()-1;
-        int j = l2.size()-1;
+//         int i = l1.size()-1;
+//         int j = l2.size()-1;
         
-        while(i >= 0 && j >= 0 && l1.get(i).val == l2.get(j).val) {
-            i--;
-            j--;
-        }
+//         while(i >= 0 && j >= 0 && l1.get(i).val == l2.get(j).val) {
+//             i--;
+//             j--;
+//         }
         
-        if(i == l1.size()-1) return null;
-        return l1.get(i+1);
+//         if(i == l1.size()-1) return null;
+//         return l1.get(i+1);
         
-    }
+//     }
     
-    public boolean isPresent(TreeNode root, int target, ArrayList<TreeNode> ans) {
-        if(root == null) return false;
-        if(root.val == target || isPresent(root.left, target, ans) || isPresent(root.right, target, ans)) {
-            ans.add(root);
-            return true;
-        }
-        return false;
+//     public boolean isPresent(TreeNode root, int target, ArrayList<TreeNode> ans) {
+//         if(root == null) return false;
+//         if(root.val == target || isPresent(root.left, target, ans) || isPresent(root.right, target, ans)) {
+//             ans.add(root);
+//             return true;
+//         }
+//         return false;
+       
+       if(root == null) return null;
+       if(root.val==p.val || root.val==q.val) return root;
+       TreeNode left=lowestCommonAncestor(root.left,p,q);
+       TreeNode right=lowestCommonAncestor(root.right,p,q);
+       if(left==null) return right;
+       if(right ==null) return left;
+       
+       return root;
+       
     }
     
     
