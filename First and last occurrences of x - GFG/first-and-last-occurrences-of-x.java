9c82384f2a0995e605e793d6fd26d576b980a012
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-01-09 06:58:16 +0530

    Added solution - LeetHub

---
+++ First and last occurrences of x - GFG/first-and-last-occurrences-of-x.java
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function Template for Java
+
+
+class GFG
+{
+    ArrayList<Long> find(long arr[], int n, int x)
+    {
+        // code here
+        ArrayList<Long> ans=new ArrayList<>();
+        long s=99999999,lg=0;
+       int get=2;
+        
+        for(int i=0;i<arr.length;i++){
+            int f=0;
+            if(arr[i]==x){
+                s=Math.min(s,i);
+                lg=Math.max(i,lg);
+                get=1;
+            }
+            
+            
+        }
+        if(get==1){
+        ans.add(s);
+        ans.add(lg);
+            
+        }
+        else{
+            ans.add((long)-1.0);
+            ans.add((long)-1.0);
+        }
+        
+        return ans;
+        
+    }
+}
+
+
+
+//{ Driver Code Starts.
+
+// Driver class
+class Array {
+
+    // Driver code
+    public static void main(String[] args) throws IOException {
+        // Taking input using buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int testcases = Integer.parseInt(br.readLine());
+        // looping through all testcases
+        while (testcases-- > 0) {
+//            int n = Integer.parseInt(br.readLine());
+            String line = br.readLine();
+            String[] q = line.trim().split("\\s+");
+            int n =Integer.parseInt(q[0]);
+            int x =Integer.parseInt(q[1]);
+//            //int y =Integer.parseInt(q[2]);
+            String line1 = br.readLine();
+            String[] a1 = line1.trim().split("\\s+");
+            long arr[] = new long[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(a1[i]);
+            }
+            GFG ob = new GFG();
+            ArrayList<Long> ans=ob.find(arr,n,x);
+            System.out.println(ans.get(0)+" "+ans.get(1));
+        }
+    }
+}
+
+// } Driver Code Ends
