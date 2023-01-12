9f9a4ec78746f9ea2fd949fdf8e92f327f4696a2
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-01-12 11:09:37 +0530

    Added solution - LeetHub

---
+++ Implement Stack using Linked List - GFG/implement-stack-using-linked-list.java
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class StackNode {
+    int data;
+    StackNode next;
+    StackNode(int a) {
+        data = a;
+        next = null;
+    }
+}
+
+class GfG {
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            MyStack obj = new MyStack();
+            int Q = sc.nextInt();
+            while (Q > 0) {
+                int QueryType = 0;
+                QueryType = sc.nextInt();
+                if (QueryType == 1) {
+                    int a = sc.nextInt();
+                    obj.push(a);
+                } else if (QueryType == 2) {
+                    System.out.print(obj.pop() + " ");
+                }
+                Q--;
+            }
+            System.out.println("");
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class MyStack 
+{
+    // class StackNode {
+    //     int data;
+    //     StackNode next;
+    //     StackNode(int a) {
+    //         data = a;
+    //         next = null;
+    //     }
+    // }   
+    StackNode top;
+    
+    //Function to push an integer into the stack.
+    void push(int a) 
+    {
+        // Add your code here
+        StackNode curr=top;
+        StackNode newNode=new StackNode(a);
+        newNode.next=top;
+        top=newNode;
+        
+        // while(curr.next!=null){
+        //     curr=curr.next;
+            
+        // }
+        // curr.next=newNode;
+        // newNode;
+        // newNode.next=null;
+        
+        
+    }
+    
+    //Function to remove an item from top of the stack.
+    int pop() 
+    {
+        // Add your code here
+        if(top==null)return -1;
+        int ans=top.data;
+        top=top.next;
+        // StackNode curr=top;
+        // // StackNode newNode=StackNode(a);
+        // while(curr.next.next!=null){
+        //     curr=curr.next;
+            
+        // }
+        // int ans=curr.next.data;
+        // curr.next=curr.next.next;
+        return ans;
+        
+    }
+}
