4e03cd9486b9b41fc5d78e89a672ed26ca3d4619
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-01-12 07:21:21 +0530

    Added solution - LeetHub

---
+++ Equilibrium Point - GFG/equilibrium-point.java
@@ -0,0 +1,64 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while (t-- > 0) {
+            
+            //taking input n
+            int n = Integer.parseInt(br.readLine().trim());
+            long arr[] = new long[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            
+            //adding elements to the array
+            for (int i = 0; i < n; i++) {
+                arr[i] = Long.parseLong(inputLine[i]);
+            }
+
+            Solution ob = new Solution();
+            
+            //calling equilibriumPoint() function
+            System.out.println(ob.equilibriumPoint(arr, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+
+    
+    // a: input array
+    // n: size of array
+    // Function to find equilibrium point in the array.
+    public static int equilibriumPoint(long arr[], int n) {
+
+        // Your code here
+        long sum = 0; // initialize sum of whole array
+        long leftsum = 0; // initialize leftsum
+ 
+        /* Find sum of the whole array */
+        for (int i = 0; i < n; ++i)
+            sum += arr[i];
+ 
+        for (int i = 0; i < n; ++i) {
+            sum -= arr[i]; // sum is now right sum for index
+                           // i
+ 
+            if (leftsum == sum)
+                return i+1;
+ 
+            leftsum += arr[i];
+        }
+ 
+        /* If no equilibrium index found, then return 0 */
+        return -1;
+    }
+}
