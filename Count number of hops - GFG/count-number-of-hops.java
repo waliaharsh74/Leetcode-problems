49682a47f7252d7d8c4394e4264ebf3371aa9ca2
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-02-18 13:56:52 +0530

    Added solution - LeetHub

---
+++ Count number of hops - GFG/count-number-of-hops.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])
+    {
+        //taking input using Scanner class
+        Scanner sc = new Scanner(System.in);
+        
+        //taking number of testcases
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            //taking number of steps in stair
+            int n = sc.nextInt();
+            
+            //calling method countWays() of class Hops
+            System.out.println(new Solution().countWays(n));
+            
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{
+    //Function to count the number of ways in which frog can reach the top.
+    static long countWays(int n)
+    {
+        // add your code here
+        long M = 1000000007;
+        long dp[]=new long[n+2];
+        dp[0]=1;
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]%M+dp[i-2]%M+dp[i-3]%M;
+        }
+        return dp[n]%M;
+    }
+    
+}
+
