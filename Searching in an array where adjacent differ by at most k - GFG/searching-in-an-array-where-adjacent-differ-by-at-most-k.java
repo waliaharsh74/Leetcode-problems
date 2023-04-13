c75c75bccd21d4468b48b5f6d6b9378886e26a6b
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-04-13 10:48:15 +0530

    Added solution - LeetHub

--- Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.java
+++ Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.java
@@ -54,8 +54,10 @@ class Complete{
     // Function for finding maximum and value pair
     public static int search (int arr[], int n, int x, int k) {
         //Complete the function
-       for(int i=0;i<n;i++){
+        int i=0;
+       while(i<n){
            if(arr[i]==x)return i;
+           i=i+Math.max(1,Math.abs((arr[i]-x)/k));
            
        }
        return -1;
