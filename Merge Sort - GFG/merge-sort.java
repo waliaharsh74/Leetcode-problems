455097f669dd8f4912627b16c887550b4284fe93
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-04-15 16:20:32 +0530

    Added solution - LeetHub

---
+++ Merge Sort - GFG/merge-sort.java
@@ -0,0 +1,124 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class Merge_Sort
+{
+    //method to print the elements of the array
+	static void printArray(int arr[])
+    {
+        StringBuffer sb=new StringBuffer("");
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            sb.append(arr[i]+" ");
+        System.out.println(sb.toString());
+    }
+
+    
+
+	public static void main(String args[])
+	{
+	    //taking input using Scanner class
+		Scanner sc = new Scanner(System.in);
+		
+		//taking testcases
+		int T = sc.nextInt();
+		while(T>0)
+		{
+		    //taking elements count
+			int n = sc.nextInt();
+			
+			//creating an object of class Merge_Sort
+			Merge_Sort ms = new Merge_Sort();
+			
+			//creating an array of size n
+			int arr[] = new int[n];
+			
+			//adding elements to the array
+			for(int i=0;i<n;i++)
+				arr[i] = sc.nextInt();
+
+            
+			Solution g = new Solution();
+			
+			//calling the method mergeSort
+			g.mergeSort(arr,0,arr.length-1);
+
+            //calling the method printArray
+			ms.printArray(arr);
+		T--;
+		}
+	}
+}
+
+
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    void merge(int arr[], int l, int m, int r)
+    {
+         // Your code here
+           int n1 = m - l + 1;
+        int n2 = r - m;
+ 
+        /* Create temp arrays */
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+ 
+        /*Copy data to temp arrays*/
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+ 
+        /* Merge the temp arrays */
+ 
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+ 
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            }
+            else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+ 
+        /* Copy remaining elements of L[] if any */
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+ 
+        /* Copy remaining elements of R[] if any */
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    void mergeSort(int arr[], int l, int r)
+    {
+        //code here
+        if (l < r) {
+            // Find the middle point
+            int m = l + (r - l) / 2;
+ 
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+ 
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+}
