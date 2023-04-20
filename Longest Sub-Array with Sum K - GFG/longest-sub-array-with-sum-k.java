fc0a4723352a6034ab0a17b48568388e36004a1a
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-04-20 20:02:14 +0530

    Added solution - LeetHub

--- Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
+++ Longest Sub-Array with Sum K - GFG/longest-sub-array-with-sum-k.java
@@ -53,34 +53,27 @@ class Solution{
     public static int lenOfLongSubarr (int a[], int n, int k) {
         //   int n = a.length; // size of the array.
 
-        Map<Integer, Integer> preSumMap = new HashMap<>();
-        int sum = 0;
-        int maxLen = 0;
-        for (int i = 0; i < n; i++) {
-            //calculate the prefix sum till index i:
-            sum += a[i];
-
-            // if the sum = k, update the maxLen:
-            if (sum == k) {
-                maxLen = Math.max(maxLen, i + 1);
-            }
-
-            // calculate the sum of remaining part i.e. x-k:
-            int rem = sum - k;
-
-            //Calculate the length and update maxLen:
-            if (preSumMap.containsKey(rem)) {
-                int len = i - preSumMap.get(rem);
-                maxLen = Math.max(maxLen, len);
-            }
-
-            //Finally, update the map checking the conditions:
-            if (!preSumMap.containsKey(sum)) {
-                preSumMap.put(sum, i);
-            }
-        }
-
-        return maxLen;
+       Map<Integer,Integer>map=new HashMap<>();
+       int sum=0;
+       int maxLen=0;
+       for(int i=0;i<n;i++){
+           sum+=a[i];
+           if(sum==k){
+               maxLen=Math.max(maxLen,i+1);
+           }
+           
+           int rem=sum-k;
+           if(map.containsKey(rem)){
+               int len=i-map.get(rem);
+               maxLen=Math.max(maxLen,len);
+               
+               
+           }
+           if(!map.containsKey(sum)){
+               map.put(sum,i);
+           }
+       }
+       return maxLen;
     }
     
     
