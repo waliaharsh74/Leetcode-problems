df8724821c60fdcd75b88e2579a859b1f753c5f8
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-05-30 15:43:27 +0530

    Added solution - LeetHub

--- Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.java
@@ -94,139 +94,40 @@ class Solution{
         
     }
     //Function to add two numbers represented by linked list.
-    static Node addTwoLists(Node l1, Node l2){
+    static Node addTwoLists(Node head1, Node head2){
         // code here
-        // return head of sum lis
-        // long ans=0,l1=0,l2=0;
-        // Node cur1=first;
-        // Node cur2=second;
-        // while(cur1!=null){
-        //     l1++;
-        //     cur1=cur1.next;
-        // }
-        // // System.out.println(l1);
-        
-        // l1=l1-1;
-        // cur1=first;
-        // while(cur1!=null){
-        //     ans+=cur1.data*Math.pow(10,l1);
-            
-        //     cur1=cur1.next;
-        //     l1--;
-        // }
-        // // System.out.println(ans);
-        
-        
-        // while(cur2!=null){
-        //     l2++;
-        //     cur2=cur2.next;
-        // }
-        // // System.out.println(l2);
-        // l2=l2-1;
-        // cur2=second;
-        // while(cur2!=null){
-        //     ans+=cur2.data*Math.pow(10.0,l2);
-        //     cur2=cur2.next;
-        //     l2--;
-        // }
-        // // System.out.println(ans);
-        // Node newHead=new Node((int)ans%10);
-        // Node cur=newHead;
-        // ans/=10;
-        // while(ans>0){
-        //     long d=ans%10;
-        //     cur.next=new Node((int)d);
-        //     cur=cur.next;
-        //     ans/=10;
-        // }
-        // // cur.next=null;
-        // return reverse(newHead);
-        
-        
-        Node prev = null;
-        // Create 3 stacks
-        Stack<Node> s1 = new Stack<Node>();
-        Stack<Node> s2 = new Stack<Node>();
-        Stack<Node> s3 = new Stack<Node>();
-        // Fill first stack with first List Elements
-        while (l1 != null) {
-            s1.add(l1);
-            l1 = l1.next;
-        }
-        // Fill second stack with second List Elements
-        while (l2 != null) {
-            s2.add(l2);
-            l2 = l2.next;
-        }
-        int carry = 0;
-        // Fill the third stack with the sum of first and
-        // second stack
-        while (!s1.isEmpty() && !s2.isEmpty()) {
-            int sum
-                = s1.peek().data + s2.peek().data + carry;
-            Node temp = new Node(sum % 10);
-            s3.add(temp);
-            if (sum > 9) {
-                carry = 1;
-            }
-            else {
-                carry = 0;
-            }
-            s1.pop();
-            s2.pop();
-        }
-        while (!s1.isEmpty()) {
-            int sum = carry + s1.peek().data;
-            Node temp = new Node(sum % 10);
-            s3.add(temp);
-            if (sum > 9) {
-                carry = 1;
-            }
-            else {
-                carry = 0;
-            }
-            s1.pop();
-        }
-        while (!s2.isEmpty()) {
-            int sum = carry + s2.peek().data;
-            Node temp = new Node(sum % 10);
-            s3.add(temp);
-            if (sum > 9) {
-                carry = 1;
-            }
-            else {
-                carry = 0;
-            }
-            s2.pop();
-        }
-        // If carry is still present create a new node with
-        // value 1 and push it to the third stack
-        if (carry == 1) {
-            Node temp = new Node(1);
-            s3.add(temp);
-        }
-        // Link all the elements inside third stack with
-        // each other
-        if (!s3.isEmpty())
-            prev = s3.peek();
-        while (!s3.isEmpty()) {
-            Node temp = s3.peek();
-            s3.pop();
-            if (s3.size() == 0) {
-                temp.next = null;
-            }
-            else {
-                temp.next = s3.peek();
-            }
-        }
-        return (prev);
-        
-        
-        
-        
-        
-        
-        
+       Node r1 = reverse(head1);
+		Node r2 = reverse(head2);
+		
+		Node head = null;
+		int carry = 0;
+		
+		while(r1 != null || r2 != null || carry == 1) {
+			int sum = carry;
+			if(r1 != null) {
+				sum += r1.data;
+				r1 = r1.next;
+			}
+			if(r2 != null) {
+				sum += r2.data;
+				r2 = r2.next;
+			}
+			
+			int digit = sum % 10;
+			carry = sum / 10;
+			
+			Node newNode = new Node(digit);
+			if(head == null) {
+				head = newNode;
+			} else {
+				newNode.next = head;
+				head = newNode;
+			}
+		}
+		return head;
+   
         
     }
+    
+    
 }
