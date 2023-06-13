4b5671f323a2965cbcfac692b5438ca4935b1ec7
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-06-13 10:21:58 +0530

    Added solution - LeetHub

---
+++ Flatten binary tree to linked list - GFG/flatten-binary-tree-to-linked-list.java
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution ob=new Solution();
+                ob.flatten(root);
+                printInorder(root);
+                System.out.println();
+                    t--;
+            
+        }
+    }
+}
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static Node prev=null;
+    public static void flatten(Node node)
+    {
+        //code here
+        
+         if (node == null)
+            return;
+        // Or if it is a leaf node
+        if (node.left == null && node.right == null)
+            return;
+        // If root.left children exists then we have to make
+        // it node.right (where node is root)
+        if (node.left != null) {
+            // Move left recursively
+            flatten(node.left);
+            // Store the node.right in Node named tempNode
+            Node tempNode = node.right;
+            node.right = node.left;
+            node.left = null;
+            // Find the position to insert the stored value
+            Node curr = node.right;
+            while (curr.right != null)
+                curr = curr.right;
+            // Insert the stored value
+            curr.right = tempNode;
+        }
+        // Now call the same function for node.right
+        flatten(node.right);
+    }
+}
