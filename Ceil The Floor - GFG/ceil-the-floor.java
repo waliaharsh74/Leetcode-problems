36fe64832ba45b2334703117ed570eea28136ea4
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-07-04 22:21:07 +0530

    Added solution - LeetHub

---
+++ Ceil The Floor - GFG/ceil-the-floor.java
@@ -0,0 +1,79 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            Pair ans = new Solve().getFloorAndCeil(arr, n, x);
+            System.out.println(ans.floor + " " + ans.ceil);
+        }
+    }
+}
+
+class Pair {
+    int floor, ceil;
+
+    Pair() {
+        this.floor = 0;
+        this.ceil = 0;
+    }
+
+    Pair(int floor, int ceil) {
+        this.floor = floor;
+        this.ceil = ceil;
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+
+
+class Solve {
+    Pair getFloorAndCeil(int[] arr, int n, int x) {
+        // code here
+        Arrays.sort(arr);
+        int s=0;
+        int f=-1;
+        int c=-1;
+        int e=n-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]==x){
+                return new Pair(arr[mid],arr[mid]);
+            }
+            else if(arr[mid]>x){
+                c=arr[mid];
+                e=mid-1;
+                
+            }
+            else{
+                f=arr[mid];
+                s=mid+1;
+            }
+        }
+        return new Pair(f,c);
+    }
+}
+
