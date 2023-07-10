6f05c8e09e0e2e2858ee22c6880b07ff470f53cf
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-07-11 00:59:44 +0530

    Added solution - LeetHub

--- Rotten Oranges - GFG/rotten-oranges.java
+++ Rotten Oranges - GFG/rotten-oranges.java
@@ -29,88 +29,66 @@ class GFG
 // } Driver Code Ends
 
 
-class Solution {
-  //Function to find minimum time required to rot all oranges. 
-  public int orangesRotting(int[][] grid) {
-    // figure out the grid size
-    int n = grid.length;
-    int m = grid[0].length;
-    // n x m 
-    Queue < Pair > q = new LinkedList < > ();
-    // n x m 
-    int[][] vis = new int[n][m];
-    int cntFresh = 0;
-
-    for (int i = 0; i < n; i++) {
-      for (int j = 0; j < m; j++) {
-        // if cell contains rotten orange
-        if (grid[i][j] == 2) {
-          q.add(new Pair(i, j, 0));
-          // mark as visited (rotten) in visited array
-          vis[i][j] = 2;
+class Solution
+{
+    //Function to find minimum time required to rot all oranges. 
+    public int orangesRotting(int[][] grid)
+    {
+        // Code here
+        int n=grid.length;
+        int m=grid[0].length;
+        int ans=0;
+        int fresh=0;
+        int cnt=0;
+        boolean vis[][]=new boolean[n][m];
+        ArrayDeque<Pair>q=new ArrayDeque<>();
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.add(new Pair(i,j,0));
+                    vis[i][j]=true;
+                }
+                if(grid[i][j]==1){
+                    fresh++;
+                }
+            }
         }
-        // if not rotten
-        else {
-          vis[i][j] = 0;
+        
+        while(!q.isEmpty()){
+            Pair cur=q.poll();
+            int r=cur.r;
+            int c=cur.c;
+            int s=cur.s;
+            ans=Math.max(ans,s);
+            
+            int delRow[]={-1,0,1,0};
+            int delCol[]={0,1,0,-1};
+            
+            for(int i=0;i<4;i++){
+                int nrow=r+delRow[i];
+                int ncol=c+delCol[i];
+                
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && !vis[nrow][ncol]){
+                    q.add(new Pair(nrow,ncol,s+1));
+                    vis[nrow][ncol]=true;
+                    cnt++;
+                }
+            }
         }
-
-        // count fresh oranges
-        if (grid[i][j] == 1) cntFresh++;
-      }
+        if(cnt==fresh)return ans;
+        return -1;
+        
+        
     }
-
-    int tm = 0;
-    // delta row and delta column
-    int drow[] = {-1, 0, +1, 0};
-    int dcol[] = {0, 1, 0, -1}; 
-    int cnt = 0;
-
-    // until the queue becomes empty
-    while (!q.isEmpty()) {
-      int r = q.peek().row;
-      int c = q.peek().col;
-      int t = q.peek().tm;
-      tm = Math.max(tm, t);
-      q.remove();
-      // exactly 4 neighbours
-      for (int i = 0; i < 4; i++) {
-        int nrow = r + drow[i];
-        int ncol = c + dcol[i];
-        // check for valid coordinates and 
-        // then for unvisited fresh orange
-        if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-          vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
-          // push in queue with timer increased
-          q.add(new Pair(nrow, ncol, t + 1));
-          // mark as rotten
-          vis[nrow][ncol] = 2;
-          cnt++;
-        }
-      }
-    }
-
-    // if all oranges are not rotten
-    if (cnt != cntFresh) return -1;
-    return tm;
-  }
-
-  public static void main(String[] args) {
-     int[][] grid =  {{0,1,2},{0,1,2},{2,1,1}};
-
-    Solution obj = new Solution();
-    int ans = obj.orangesRotting(grid);
-    System.out.println(ans);
-  }
-
 }
-
-class Pair {
-  int row;
-  int col;
-  int tm;
-  Pair(int _row, int _col, int _tm) {
-    this.row = _row;
-    this.col = _col;
-    this.tm = _tm;
-  }
+class Pair{
+    int r;
+    int c;
+    int s;
+    Pair(int r,int c,int s){
+        this.r=r;
+        this.c=c;
+        this.s=s;
+    }
 }
