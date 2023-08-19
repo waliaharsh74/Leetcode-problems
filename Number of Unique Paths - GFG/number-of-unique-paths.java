92e2a155083fd45631eec503bd99eb9adcda75c1
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-08-19 19:35:33 +0530

    Added solution - LeetHub

---
+++ Number of Unique Paths - GFG/number-of-unique-paths.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+	public static void main (String[] args) {
+	    
+	    //taking input using Scanner class
+		Scanner sc=new Scanner(System.in);
+		
+		//taking total testcases
+		int t=sc.nextInt();
+		while(t-->0)
+		{
+		    //taking dimensions of the matrix
+		    int a=sc.nextInt();
+		    int b=sc.nextInt();
+		    Solution ob = new Solution();
+		    //calling method NumberOfPath()
+		    System.out.println(ob.NumberOfPath(a,b));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find total number of unique paths.
+    public static int NumberOfPath(int a, int b) 
+    {
+        //Your code here
+         int dp[][]=new int[a][b];
+    for (int[] row : dp)
+        Arrays.fill(row, -1);
+         return countWaysUtil(a-1,b-1,dp);
+        
+        
+    }
+    static int countWaysUtil(int i, int j, int[][] dp) {
+  if(i==0 && j == 0)
+    return 1;
+  if(i<0 || j<0)
+    return 0;
+  if(dp[i][j]!=-1) return dp[i][j];
+    
+  int up = countWaysUtil(i-1,j,dp);
+  int left = countWaysUtil(i,j-1,dp);
+  
+  return dp[i][j] = up+left;
+  
+}
+}
