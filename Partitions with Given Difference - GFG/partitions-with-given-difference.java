06950c0eec54693f022998247625b53800cd1b12
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-08-29 01:25:22 +0530

    Added solution - LeetHub

---
+++ Partitions with Given Difference - GFG/partitions-with-given-difference.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+    public static void main(String args[]) throws IOException{
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0){
+            int n=sc.nextInt();
+            int d=sc.nextInt();
+            
+            int []arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=sc.nextInt();
+            }
+            
+            Solution obj=new Solution();
+            int res=obj.countPartitions(n, d, arr);
+            System.out.println(res);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+//Back-end complete function Template for Java
+
+class Solution{
+
+    public int countPartitions(int n, int d, int arr[]){
+        // Code here
+        // int n = num.length;
+//   int n = arr.length;
+    int totSum = 0;
+    for(int i=0; i<arr.length;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d<0) return 0;
+    if((totSum-d)%2==1) return 0;
+    
+    int s2 = (totSum-d)/2;
+    
+    int dp[][] = new int[n][s2+1];
+    
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    
+    return findWaysUtil(n-1,s2,arr,dp);
+        
+        // if()
+        
+        
+        
+    }
+    static int findWaysUtil(int ind, int target,int[] arr, int[][] dp){
+        int mod=1000000007;
+
+    if(ind == 0){
+            if(target==0 && arr[0]==0)
+                return 2;
+            if(target==0 || target == arr[0])
+                return 1;
+            return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = findWaysUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = findWaysUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken)%mod;
+}
+    
+}
