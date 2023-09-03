26bfd42ae65fb68e7d07262767b6747288260e7e
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-09-04 01:12:26 +0530

    Added solution - LeetHub

---
+++ Wildcard Pattern Matching - GFG/wildcard-pattern-matching.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class WildcardPattern
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		sc.nextLine();
+		while(t>0)
+		{
+			String pat = sc.nextLine();
+			String text = sc.nextLine();
+			Solution g = new Solution();
+			System.out.println(g.wildCard(pat,text));
+			t--;
+		}
+	}
+}
+// } Driver Code Ends
+
+
+class Solution
+{
+    int wildCard(String p, String s)
+    {
+        // Your code goes here
+        int n = s.length();
+        int m = p.length();
+        boolean dp[][] = new boolean[n + 1][m + 1];
+
+        dp[0][0] = true;
+
+        for (int i = 1; i <= m; i++) {
+            if (p.charAt(i - 1) == '*') {
+                dp[0][i] = dp[0][i - 1];
+            }
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return (dp[n][m])?1:0;
+    }
+}
