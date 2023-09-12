38c65faa215a2af92910f1723aa4776076b5db1f
Harsh Walia <89077517+waliaharsh74@users.noreply.github.com>
2023-09-12 16:30:37 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.java
@@ -52,32 +52,26 @@ class Solution
     static int knapSack(int W, int wt[], int val[], int n) 
     { 
          // your code here 
-        int dp[][]=new int[n][W+1];
-        for(int i=0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        int dp[][]=new int [n][W+1];
+        for(int i=wt[0];i<=W;i++){
+            dp[0][i]=val[0];
         }
-        return mem(W,wt,val,n-1,dp);
+        // if(wt[0]<=W){
+        //     dp[0][W-wt[0]]=val[0];
+        // }
         
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=W;j++){
+                int np=dp[i-1][j];
+                int p=Integer.MIN_VALUE;
+                if(wt[i]<=j){
+                    p=val[i]+dp[i-1][j-wt[i]];
+                }
+                dp[i][j]=Math.max(p,np);
+            }
+        }
+        return dp[n-1][W];
     } 
-    
-    static int mem(int W, int wt[], int val[],int i,int dp[][]){
-        if(i==0){
-            if(wt[i]<=W){
-                return val[i];
-            }
-            else{
-                return 0;
-            }
-        }
-        if(dp[i][W]!=-1)return dp[i][W];
-        int notake=mem(W,wt,val,i-1,dp);
-        int take=Integer.MIN_VALUE;
-        if(wt[i]<=W){
-            take=val[i]+mem(W-wt[i],wt,val,i-1,dp);
-        }
-        return dp[i][W]=Math.max(take,notake);
-    }
 }
-
 
 
